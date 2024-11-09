67635496c42a61cb8c112de4f31a9a3b32f3173d
Jose Rodriguez <jrodriguez@denwaip.com>
2024-11-09 19:51:58 -0300

    fix port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
