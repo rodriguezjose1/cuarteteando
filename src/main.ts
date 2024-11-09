5ea0c64f5c25e02cce479c2503233d5a54e5edb9
Jose Rodriguez <jrodriguez@denwaip.com>
2024-11-09 20:28:15 -0300

    set hostname in listen

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
